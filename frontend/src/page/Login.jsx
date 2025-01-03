359eaabbc1b74a519162fd0decb5dbf2c095c8a1
hydrazin60 <sureshkhdk45@gmail.com>
2025-01-03 21:27:16 +0545

    Create Login API and connect on frontend

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -1,14 +1,69 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-
+import { IoEyeOffOutline } from "react-icons/io5";
+import { IoEyeOutline } from "react-icons/io5";
+import { toast } from "sonner";
+import axios from "axios";
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoding, setIsLoding] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = async (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return toast.error("All fields are required");
+    }
+    try {
+      setIsLoding(true);
+      const res = await axios.post(
+        "http://localhost:4000/api/v1/amazoneClone/user/auth/login",
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setIsLoding(false);
+      if (res.data.success) {
+        toast.success(res.data.message || "Login successful");
+        navigate("/");
+        setFormData({
+          email: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      setIsLoding(false);
+      if (error.response) {
+        console.error("Server Error:", error.response.data);
+        toast.error(error.response.data.message || "Server error occurred.");
+      } else if (error.request) {
+        console.error("Network Error:", error.request);
+        toast.error("Network error. Please check your internet connection.");
+      } else {
+        console.error("Unexpected Error:", error.message);
+        toast.error("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
   const navigate = useNavigate();
   return (
-    <body className="w-screen h-screen p-3 gap-7 flex  flex-col items-center  ">
-      <section className="w-[350px] flex flex-col gap-5  " >
+    <main className="w-screen h-screen p-3 gap-7 flex  flex-col items-center  ">
+      <section className="w-[350px] flex flex-col gap-5  ">
         <div>
           <img
             src="https://assets.aboutamazon.com/2e/d7/ac71f1f344c39f8949f48fc89e71/amazon-logo-squid-ink-smile-orange.png"
@@ -19,17 +74,65 @@ export default function Login() {
         <div className="border border-slate-300 rounded-md p-6 ">
           <div className="flex flex-col gap-3 ">
             <div>
-              <h2 className="text-2xl font-semibold text-zinc-800 " >Sign in</h2>
+              <h2 className="text-2xl font-semibold text-zinc-800 ">Sign in</h2>
             </div>
-            <div>
-              <label for="email" className="text-sm font-bold text-zinc-800" >
-                Email or mobile phone number
-              </label>
-              <Input className="border border-slate-400    " type="email" />
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="text-sm font-bold text-zinc-800"
+                >
+                  Email or mobile phone number
+                </label>
+                <Input
+                  type="email"
+                  name="email"
+                  required
+                  value={formData.email}
+                  placeholder="Email or mobile phone number"
+                  className="border border-slate-400"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="relative">
+                <label
+                  htmlFor="password"
+                  className="text-sm font-bold text-zinc-800"
+                >
+                  Password
+                </label>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  required
+                  value={formData.password}
+                  placeholder="password at least 6 characters"
+                  className="border border-slate-400"
+                  onChange={handleChange}
+                />
+                {showPassword ? (
+                  <IoEyeOutline
+                    className="absolute cursor-pointer right-2 top-11 transform -translate-y-1/2 text-gray-600"
+                    onClick={() => setShowPassword(false)}
+                  />
+                ) : (
+                  <IoEyeOffOutline
+                    className="absolute right-2 cursor-pointer top-11 transform -translate-y-1/2 text-gray-600"
+                    onClick={() => setShowPassword(true)}
+                  />
+                )}
+              </div>
               <Button className="w-full my-3 bg-yellow-400 hover:bg-yellow-500 text-black rounded-full">
-                Continue
+                {isLoding ? (
+                  <div className="flex items-center justify-center">
+                    <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-gray-900"></div>
+                    <span className="ml-2">Loading...</span>
+                  </div>
+                ) : (
+                  "Continue"
+                )}
               </Button>
-            </div>
+            </form>
             <div>
               <div>
                 <p className="text-[13px]">
@@ -118,6 +221,6 @@ export default function Login() {
           </div>
         </div>
       </section>
-    </body>
+    </main>
   );
 }
--- server/controller/auth/user.auth.js
+++ server/controller/auth/user.auth.js
@@ -41,4 +41,57 @@ export const registerUser = async (req, res) => {
   }
 };
 
- 
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Please fill all the fields",
+      });
+    }
+    const existUser = await Usermodel.findOne({ email });
+    if (!existUser) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "User does not exist! Please Go to register page",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, existUser.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "Password is incorrect! Please try again",
+      });
+    }
+    const userData = existUser.toObject();
+    delete userData.password;
+    const token = jwt.sign({ id: existUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        samesite: "none",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        success: true,
+        error: false,
+        message: `Welcome ${existUser.fullName} in our website you can now shop`,
+        data: userData,
+      });
+  } catch (error) {
+    console.log(`Error occured in Login User ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: `Error occured in Login User ${error}`,
+    });
+  }
+};
