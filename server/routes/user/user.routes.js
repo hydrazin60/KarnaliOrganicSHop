5eb4797b7ea60707a2fcdeabd9983de3c17b7b24
hydrazin60 <sureshkhdk45@gmail.com>
2025-02-08 21:27:05 +0545

    viewOwnOrder API create and check

--- server/routes/user/user.routes.js
+++ server/routes/user/user.routes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import {
   getUserDetails,
+  processOrder,
   ownProfileView,
+  viewOwnOrder,
 } from "../../controller/user/user.controller.js";
 import { isAuthenticated } from "../../middleware/isAuthenticated.js";
-
 const userRouter = express.Router();
 
 userRouter.get("/get_user_details/:userId", getUserDetails);
 userRouter.get("/get_own_profile", isAuthenticated, ownProfileView);
+userRouter.post("/place_order/:orderId", isAuthenticated, processOrder);
+userRouter.get("/viewAll_own_orders", isAuthenticated, viewOwnOrder);
 
 export default userRouter;
