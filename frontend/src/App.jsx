b5944fe18fa1fcd7b350ebc01771913304be40b2
hydrazin60 <sureshkhdk45@gmail.com>
2025-04-02 20:52:45 +0545

    hh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 import Login from "./page/Login";
-import { Divide } from "lucide-react";
+
 import Register from "./page/Register";
-// import SpecificsProduct from "./page/product/specificsProduct";
 import UserDashbord from "./page/UserDashbord";
-import ProductUpload from "./page/Admin/ProductUpload";
-import ManageUsers from "./page/Admin/ManageUsers";
-import ManageProduct from "./page/Admin/ManageProduct";
-import SingleProductPage from "./page/product/SingleProductPage";
-import BarChartComponent from "./components/Admin/Dashbord";
+
 import Analisics from "./components/Admin/Analisics";
- 
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -23,21 +18,12 @@ export default function App() {
           path="/"
           element={
             <>
-           
               <Header />
               <Home />
             </>
           }
         />
-        <Route
-          path="/specificsProduct/:id"
-          element={
-            <>
-              <Header />
-              {/* <SpecificsProduct /> */}
-            </>
-          }
-        />
+
         <Route
           path="/user/dashbord/:id"
           element={
@@ -47,33 +33,7 @@ export default function App() {
             </>
           }
         />
-        <Route
-          path="/admin/product/upload/:id"
-          element={
-            <>
-              <Header />
-              <ProductUpload />
-            </>
-          }
-        />
-        <Route
-          path="/admin/manage_users/:id"
-          element={
-            <>
-              <Header />
-              <ManageUsers />
-            </>
-          }
-        />
-        <Route
-          path="/admin/product_manage/:id"
-          element={
-            <>
-              <Header />
-              <ManageProduct />
-            </>
-          }
-        />
+
         <Route
           path="/sell/dashbord"
           element={
@@ -83,15 +43,7 @@ export default function App() {
             </>
           }
         />
-        <Route
-          path="/user/see_single_product/:id"
-          element={
-            <>
-              <Header />
-              <SingleProductPage />
-            </>
-          }
-        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
