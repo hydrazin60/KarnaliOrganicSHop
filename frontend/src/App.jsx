35f1bac263bba3deaf363b58bd0b2362cffb080d
hydrazin60 <sureshkhdk45@gmail.com>
2025-04-02 21:02:18 +0545

    Fixed file name casing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 import Login from "./page/Login";
-
+import { Divide } from "lucide-react";
 import Register from "./page/Register";
+import SpecificsProduct from "./page/product/specificsProduct";
 import UserDashbord from "./page/UserDashbord";
-
+import ProductUpload from "./page/Admin/ProductUpload";
+import ManageUsers from "./page/Admin/ManageUsers";
+import ManageProduct from "./page/Admin/ManageProduct";
+import SingleProductPage from "./page/product/SingleProductPage";
 import Analisics from "./components/Admin/Analisics";
+import AttendanceDashboard from "./components/Admin/AdminAccountDashboard";
 
 export default function App() {
   return (
@@ -18,12 +23,21 @@ export default function App() {
           path="/"
           element={
             <>
+              <AttendanceDashboard />
               <Header />
               <Home />
             </>
           }
         />
-
+        <Route
+          path="/specificsProduct/:id"
+          element={
+            <>
+              <Header />
+              <SpecificsProduct />
+            </>
+          }
+        />
         <Route
           path="/user/dashbord/:id"
           element={
@@ -33,7 +47,33 @@ export default function App() {
             </>
           }
         />
-
+        <Route
+          path="/admin/product/upload/:id"
+          element={
+            <>
+              <Header />
+              <ProductUpload />
+            </>
+          }
+        />
+        <Route
+          path="/admin/manage_users/:id"
+          element={
+            <>
+              <Header />
+              <ManageUsers />
+            </>
+          }
+        />
+        <Route
+          path="/admin/product_manage/:id"
+          element={
+            <>
+              <Header />
+              <ManageProduct />
+            </>
+          }
+        />
         <Route
           path="/sell/dashbord"
           element={
@@ -43,7 +83,15 @@ export default function App() {
             </>
           }
         />
-
+        <Route
+          path="/user/see_single_product/:id"
+          element={
+            <>
+              <Header />
+              <SingleProductPage />
+            </>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
